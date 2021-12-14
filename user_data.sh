765b707b010f7763101f74a45b86e8154ffc2377
xiehuangjun <60767398+xiehuangjun@users.noreply.github.com>
2021-12-14 15:06:41 +0800

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -4,7 +4,7 @@ sudo yum install git -y
 cd /home/ec2-user/
 sudo git clone https://github.com/xiehuangjun/A3.git
 cd /home/ec2-user/A3/API/
-sudo sed -i -e 's/host_value//g' -e 's/port_value/3306/g' -e 's/user_value/root/g' -e 's/password_value/12345678/g' config.ini
+sudo sed -i -e 's/host_value/[Database's name]/g' -e 's/port_value/[Port]/g' -e 's/user_value/[User]/g' -e 's/password_value/[Password]/g' config.ini
 sudo yum install mysql-devel gcc python-pip python-devel -y
 sudo pip3 install PyMySQL
 cd /home/ec2-user/A3/API/create_table
